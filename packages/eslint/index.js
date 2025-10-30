5d02a493c5d60aa6225e76639b7ce393b87d057d
viniciussis <vinicius01012@gmail.com>
2025-10-30 11:27:16 -0300

    chore: add git and docker ignore files

--- packages/eslint/index.js
+++
@@ -1,6 +0,0 @@
-import typescript from './typescript';
-import react from './react';
-import base from './base';
-import node from './node';
-
-export default [...base, ...node, ...react, ...typescript];
