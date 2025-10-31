311d734f97d02fc435321d41801d2769bcba4511
Vinícius Silva Santos <vinicius01012@gmail.com>
2025-10-30 22:31:29 -0300

    chore(api): wire up new modules and update dependencies

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,6 +1,11 @@
+import { DatabaseModule } from './plugins/database/database.module';
+import { TmdbService } from './modules/tmdb/tmdb.service';
+import { TmdbModule } from './modules/tmdb/tmdb.module';
 import { configuration } from './config/configuration';
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+import { MoviesModule } from './modules/movies/movies.module';
+import { FavoritesModule } from './modules/favorites/favorites.module';
 
 @Module({
   imports: [
@@ -8,6 +13,11 @@ import { Module } from '@nestjs/common';
       isGlobal: true,
       load: [configuration],
     }),
+    DatabaseModule,
+    TmdbModule,
+    MoviesModule,
+    FavoritesModule,
   ],
+  providers: [TmdbService],
 })
 export class AppModule {}
