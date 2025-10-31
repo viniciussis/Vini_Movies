1069d7f0b6deddfdaf04d5f9796c89fb5a0830e4
Vinícius Silva Santos <vinicius01012@gmail.com>
2025-10-30 22:31:15 -0300

    feat(api): setup prisma service and database module

--- apps/api/prisma.config.ts
+++ apps/api/prisma.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig, env } from 'prisma/config';
+import * as path from 'path';
+import 'dotenv/config';
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
+  schema: path.join(__dirname, 'prisma/schema.prisma'),
   migrations: {
     path: 'prisma/migrations',
   },
---
+++ apps/api/src/plugins/database/database.module.ts
@@ -0,0 +1,9 @@
+import { PrismaService } from './services/prisma.service';
+import { Global, Module } from '@nestjs/common';
+
+@Global()
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService],
+})
+export class DatabaseModule {}
